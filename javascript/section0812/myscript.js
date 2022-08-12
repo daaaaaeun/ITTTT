8b4d107ce86e5cbccbd895f9d1e23a9d5331ffe3
Choi daeun <ed0408202@gmail.com>
2022-08-12 09:54:14 +0900

    0812

--- javascript/section0812/myscript.js
+++ javascript/section0812/myscript.js
@@ -4,17 +4,29 @@ function hap(a, b){//두수사이의 합을 반환하시오
     //2+3+4+5
     //5+4+3+2
 
-    for(){
-
+    if(a>b){
+        let tmp=a;  a=b;    b=tmp;
     }
 
+    let sum=0;
+    for(let n=a; n<=b; n++){
+        sum += n;
+    }
+
+    return sum;
 }//hap() end
 
 function diff(a, b){//두수사이의 차이를 반환하시오
     //2, 5
     //5, 2
+
+    return Math.abs(a-b);
 }//diff end
 
 function leap(year){//윤년, 평년을 반환하시오
-
+    if(year%4==0 && year%100!=0 || year%400==0){
+        return true;
+    }else{
+        return false;
+    }
 }//leap() end
